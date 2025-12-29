77684cc118be94580c6af3dc31aadd1a5b74fa6d
zhen <zhen@Mac.Home>
2025-12-29 11:06:35 +0000

    upgrade to 0.1.0 complete

--- got_bullet/main.go
+++ got_bullet/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	boldtb "github.com/vixac/bullet/store/boltdb"
+	"github.com/vixac/bullet/store/store_interface"
 	local_bullet "github.com/vixac/firbolg_clients/bullet/local_bullet"
 	"vixac.com/got/cmd"
 	"vixac.com/got/console"
@@ -13,16 +14,32 @@ import (
 
 func main() {
 	path := os.Getenv("GOT_BOLT")
+	//fmt.Printf("VX: LOOKING AT PATH %s\n", path)
 	if path == "" {
 		log.Fatal("missing env GOT_BOLT, which should be the path to the got bolt file")
 	}
-	boltdb, err := boldtb.NewBoltStore(path)
+	bolt, err := boldtb.NewBoltStore(path)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	space := store_interface.TenancySpace{
+		AppId:     123,
+		TenancyId: 0,
+	}
+
+	/*
+		bolt.MigrateToTenantBuckets(space, 1001)
+		bolt.MigrateToTenantBuckets(space, 100)
+		bolt.MigrateToTenantBuckets(space, 1002)
+		bolt.MigrateToTenantBuckets(space, 1003)
+		bolt.MigrateToTenantBuckets(space, 1004)
+		bolt.MigrateToTenantBuckets(space, 0)
+		bolt.MigrateToTenantBuckets(space, 1005)
+	*/
 	localBullet := local_bullet.LocalBullet{
-		AppId: 123,
-		Store: boltdb,
+		Space: space,
+		Store: bolt,
 	}
 
 	ene, err := bullet_engine.NewEngineBullet(&localBullet)
