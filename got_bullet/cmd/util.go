23f15cc165ae56ac2b31d1d462381f8cbb31fddd
vic <vic@vixac.com>
2026-01-29 20:45:41 +0000

    pasted

--- got_bullet/cmd/util.go
+++ got_bullet/cmd/util.go
@@ -1,10 +1,52 @@
 package cmd
 
 import (
+	"fmt"
+	"time"
+
 	"vixac.com/got/console"
 	"vixac.com/got/engine"
 	"vixac.com/got/engine/bullet_engine"
 )
+
+func (e *EngineBullet) addDailyDividers(items []engine.GotItemDisplay) []engine.GotItemDisplay {
+
+	var finalResult []engine.GotItemDisplay
+	//VX:TODO go to midnight instead of now:
+
+	midnight := LastMidnightUTC()
+	today := midnight.Unix()
+	yesterday := midnight.AddDate(0, 0, -1).Unix()
+	lastWeek := midnight.AddDate(0, 0, -7).Unix()
+
+	var lastWeekInPlace = false
+	var yesterdayInPlace = false
+	var todayInPlace = false
+	for _, i := range items {
+		dateOfThisItem, err := i.SummaryObj.UpdatedDate.ToDate()
+		timeOfThisItem := time.Time(*dateOfThisItem).Unix()
+		//itemDate := Date(dateOfThisItemStr)
+		if err != nil {
+			fmt.Printf("Error dating this item. Not adding daily dividers. This is a quiet failure.")
+			return items
+		}
+		if !lastWeekInPlace {
+			if timeOfThisItem > lastWeek && timeOfThisItem < yesterday {
+
+				lastWeekInPlace = true
+
+			}
+
+		} else if !yesterdayInPlace {
+
+		} else if !todayInPlace {
+
+		}
+		finalResult = append(finalResult, i)
+	}
+	return finalResult
+
+}
 
 func renderTable(lookup *engine.GidLookup, states []engine.GotState, options bullet_engine.TableRenderOptions, deps RootDependencies) {
 	res, err := deps.Engine.FetchItemsBelow(lookup, options.SortByPath, states)
@@ -19,12 +61,18 @@ func renderTable(lookup *engine.GidLookup, states []engine.GotState, options bul
 
 	var sections [][]engine.GotItemDisplay
 	if options.GroupByTimeFrame {
+
+		midnight := LastMidnightUTC()
+		todayTime := midnight.Unix()
+		yesterday := midnight.AddDate(0, 0, -1).Unix()
+		lastWeek := midnight.AddDate(0, 0, -7).Unix()
 		//VX:TODO
-		var today []engine.GotItemDisplay
-		var yesterday []engine.GotItemDisplay
-		var lastWeek []engine.GotItemDisplay
-		var theRest []engine.GotItemDisplay
+		var todayItems []engine.GotItemDisplay
+		var yesterdayItems []engine.GotItemDisplay
+		var lastWeekItems []engine.GotItemDisplay
+		var theRestItems []engine.GotItemDisplay
 		for _, r := range res.Result {
+			r.SummaryObj.UpdatedDate
 
 		}
 	} else {
